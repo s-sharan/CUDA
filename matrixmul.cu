a61f90f107f91c412087f72cf3dc4b447516e080
s-sharan <sharan@MacBook-Pro.fios-router.home>
2016-04-02 16:55:05 -0400

    Modifications to matrixmul

--- matrixmul.cu
+++ matrixmul.cu
@@ -1,3 +1,6 @@
+#include <random>
+#include <chrono>
+#include <limits>
 #include <iostream>
 #include <vector>
 #include <stdlib.h>
@@ -9,13 +12,64 @@
 #include <math.h>
 
 using namespace std;
+using namespace std::chrono;
+
+__global__ void matrixMultiplicationKernel(float* A, float* B, float* C, int N) {
+
+    int ROW = blockIdx.y*blockDim.y+threadIdx.y;
+    int COL = blockIdx.x*blockDim.x+threadIdx.x;
+
+    float tmpSum = 0;
+
+    if (ROW < N && COL < N) {
+        // each thread computes one element of the block sub-matrix
+        for (int i = 0; i < N; i++) {
+            tmpSum += A[ROW * N + i] * B[i * N + COL];
+        }
+    }
+    C[ROW * N + COL] = tmpSum;
+}
+
+
+void matrixMultiplication(float *A, float *B, float *C, int N){
+
+    // declare the number of blocks per grid and the number of threads per block
+    // use 1 to 512 threads per block
+    dim3 threadsPerBlock(N, N);
+    dim3 blocksPerGrid(1, 1);
+        if (N*N > 512){
+            threadsPerBlock.x = 512;
+            threadsPerBlock.y = 512;
+            blocksPerGrid.x = ceil(double(N)/double(threadsPerBlock.x));
+            blocksPerGrid.y = ceil(double(N)/double(threadsPerBlock.y));
+        }
+
+    matrixMultiplicationKernel<<<blocksPerGrid,threadsPerBlock>>>(A, B, C, N);
+}
+
 
 int main()
 {
     // Perform matrix multiplication C = A*B
     // where A, B and C are NxN matrices
-    int N = 16;
+    //int N = 16;
+    int N;
+    cout<<"Enter the size of the arrays N:"
+    cin>>N;
     int SIZE = N*N;
+
+    // Using Uniform Random number generator to initialize the arrays.
+    int max_int = numeric_limits<int>::max();
+  	default_random_engine generator(seed);
+  	uniform_int_distribution<int> distribution(0,max_int-1);
+
+  	// Making use of steady clock to measure the amount of time taken to compute the product of matrices.
+  	steady_clock::time_point gpu_start;
+    steady_clock::time_point gpu_end;
+    steady_clock::time_point cpu_start;
+    steady_clock::time_point cpu_end;
+    duration<double> gpu_time_span;
+    duration<double> cpu_time_span;
 
     // Allocate memory on the host
     vector<float> h_A(SIZE);
@@ -25,8 +79,8 @@ int main()
     // Initialize matrices on the host
     for (int i=0; i<N; i++){
         for (int j=0; j<N; j++){
-            h_A[i*N+j] = sin(i);
-            h_B[i*N+j] = cos(j);
+            h_A[i*N+j] = distribution(generator);
+            h_B[i*N+j] = distribution(generator);
         }
     }
 
@@ -38,16 +92,21 @@ int main()
     d_A.set(&h_A[0], SIZE);
     d_B.set(&h_B[0], SIZE);
 
+    gpu_start = steady_clock::now();
     matrixMultiplication(d_A.getData(), d_B.getData(), d_C.getData(), N);
     cudaDeviceSynchronize();
+    
 
     d_C.get(&h_C[0], SIZE);
     cudaDeviceSynchronize();
+    gpu_end = steady_clock::now();
+    gpu_time_span = duration_cast<duration<double>>(gpu_end - gpu_start);
 
     float *cpu_C;
     cpu_C=new float[SIZE];
 
     // Now do the matrix multiplication on the CPU
+    cpu_start = steady_clock::now();
     float sum;
     for (int row=0; row<N; row++){
         for (int col=0; col<N; col++){
@@ -58,6 +117,11 @@ int main()
             cpu_C[row*N+col] = sum;
         }
     }
+    cpu_end = steady_clock::now();
+    cpu_time_span = duration_cast<duration<double>>(cpu_end - cpu_start);
+
+    cout<<"Time taken to compute the product on a GPU: "<<gpu_time_span.count()<<endl;
+    cout<<"Time taken to compute the product on a CPU: "<<cpu_time_span.count()<<endl;
 
     double err = 0;
     // Check the result and make sure it is correct
