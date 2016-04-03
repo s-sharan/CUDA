cb9b83e01c38bf5f2e8b64d7a3a9f38d1f8896fe
Ubuntu <ubuntu@ip-172-31-0-218.ec2.internal>
2016-04-03 00:59:06 +0000

    Matrix transpose

--- matrixTranspose.cu
+++ matrixTranspose.cu
@@ -9,29 +9,28 @@
 #include <math.h>
 #include <fstream>
 
+#define TILE_DIM 32
+#define BLOCK_ROWS 8
 using namespace std;
 using namespace std::chrono;
 
 __global__ void transposeCoalescedKernel(float *odata, const float *idata, int N)
 {
-  const int size=64;
-  int rows=N/size;
-  __shared__ float tile[size][size];
+  __shared__ float tile[TILE_DIM][TILE_DIM];
 
+  int x = blockIdx.x * TILE_DIM + threadIdx.x;
+  int y = blockIdx.y * TILE_DIM + threadIdx.y;
+  int width = gridDim.x * TILE_DIM;
 
-  int x = blockIdx.x * size + threadIdx.x;
-  int y = blockIdx.y * size + threadIdx.y;
-  int width = gridDim.x * size;
-
-  for (int j = 0; j < size; j += rows)
+  for (int j = 0; j < TILE_DIM; j += BLOCK_ROWS)
      tile[threadIdx.y+j][threadIdx.x] = idata[(y+j)*width + x];
 
   __syncthreads();
 
-  x = blockIdx.y * size + threadIdx.x;  // transpose block offset
-  y = blockIdx.x * size + threadIdx.y;
+  x = blockIdx.y * TILE_DIM + threadIdx.x;  // transpose block offset
+  y = blockIdx.x * TILE_DIM + threadIdx.y;
 
-  for (int j = 0; j < size; j += rows)
+  for (int j = 0; j < TILE_DIM; j += BLOCK_ROWS)
      odata[(y+j)*width + x] = tile[threadIdx.x][threadIdx.y + j];
 }
 
@@ -39,15 +38,8 @@ void transposeCoalesced(float *odata, const float *idata, int N){
     // declare the number of blocks per grid and the number of threads per block
     // use 1 to 512 threads per block. 
     // a maximum of 512 threads can be assigned to a block
-    dim3 threadsPerBlock(N, N);
-    dim3 blocksPerGrid(1, 1);
-        if (N*N > 512){
-            threadsPerBlock.x = 512;
-            threadsPerBlock.y = 512;
-            blocksPerGrid.x = ceil(double(N)/double(threadsPerBlock.x));
-            blocksPerGrid.y = ceil(double(N)/double(threadsPerBlock.y));
-        }
-    transposeCoalescedKernel<<<blocksPerGrid,threadsPerBlock>>>(odata,idata, N);
+    dim3 grid(N/TILE_DIM, N/TILE_DIM),threads(TILE_DIM,BLOCK_ROWS);
+    transposeCoalescedKernel<<<grid,threads>>>(odata,idata, N);
 }
 
 
@@ -66,7 +58,7 @@ int main()
     if(ch=='y') flag=true;
 
     // Using Uniform Random number generator to initialize the arrays.
-  	default_random_engine generator(12312);
+  	default_random_engine generator(2321);
   	uniform_real_distribution<> distribution(-10.0,10.0);
 
   	// Making use of steady clock to measure the amount of time taken to compute the product of matrices.
@@ -96,7 +88,7 @@ int main()
     d_A.set(&h_A[0], SIZE);
 
 
-
+    
     gpu_start = steady_clock::now();
     transposeCoalesced(d_B.getData(), d_A.getData(),N);
     cudaDeviceSynchronize();
@@ -105,27 +97,19 @@ int main()
     cudaDeviceSynchronize();
     gpu_end = steady_clock::now();
     gpu_time_span = duration_cast<duration<double>>(gpu_end - gpu_start);
+    
+    
 
     cout<<"Time taken to compute the product on a GPU: "<<gpu_time_span.count()<<endl;
-    double err = 0;
     float *cpu_B;
     if(flag){
 	    cpu_B=new float[SIZE];
-      for (int row=0; row<N; row++){
-          for (int col=0; col<N; col++){
-              cout<<h_A[row*N+col]<<"       ";
-          }
-          cout<<endl;
-      }
-
 	    // Now do the matrix multiplication on the CPU
 	    cpu_start = steady_clock::now();
 	    for (int row=0; row<N; row++){
 	        for (int col=0; col<N; col++){
 	            cpu_B[row*N+col]=h_A[col*N+row];
-              cout<<cpu_B[row*N+col]<<"       ";
-	        }
-          cout<<endl;
+            }
 	    }
 	    cpu_end = steady_clock::now();
 	    cpu_time_span = duration_cast<duration<double>>(cpu_end - cpu_start);
@@ -139,9 +123,9 @@ int main()
 
 	for (int ROW=0; ROW < N; ROW++){
         for (int COL=0; COL < N; COL++){
-    		matA<<h_A[ROW * N + COL]<"\t";
-    		if(flag) cpu<<cpu_B[ROW * N + COL]<"\t";
-            gpu<<h_B[ROW * N + COL]<<"\t";
+    		  matA<<h_A[ROW * N + COL]<<"\t";
+    		  if(flag)cpu<<cpu_B[ROW * N + COL]<<"\t";
+          gpu<<h_B[ROW * N + COL]<<"\t";
         }
         if(flag) cpu<<endl;
         gpu<<endl;
@@ -150,3 +134,4 @@ int main()
 
     return 0;
 }
+
